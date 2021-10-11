d428d4d991a3e9878c0832525eaa49e572c44a77
pooja <randivepooja26@gmail.com>
2021-10-11 13:40:11 +0530

    header upadated

--- header.php
+++ header.php
@@ -27,7 +27,7 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentyseventeen' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
+	<header id="masthead" class="site-header site-header-new" role="banner">
 
 		<?php get_template_part( 'template-parts/header/header', 'image' ); ?>
 
